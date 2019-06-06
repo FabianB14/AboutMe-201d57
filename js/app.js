86e722af618b3f719ab012687b71fdd392ed7228
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-06-06 00:36:25 -0700

    This is the 2nd update to Fabian's about me page.

---
+++ js/app.js
@@ -0,0 +1,116 @@
+'use strict';
+//This is a count var to keep count of how many answers are right and how many are worng
+var countRight = 0;
+var countWrong = 0;
+var flag = false;
+var langAnswer = 'yes';
+var heightAnswer = 'no';
+var loveCodingAnswer = 'yes';
+var locationAnswer = 'yes';
+var laptopAnswer = 'no';
+var langQuestion = 'Do you believe I can speak another language?';
+var heightQuestion = 'Do you think I am 7ft tall?';
+var loveCodingQuestion = 'From what you have seen so far do you believe I am a guy that loves coding?';
+var locationQuestion = 'Do you think I\'m from New Orleans';
+var laptopQuestion = 'Do you think I am using a Macbook pro as my laptop?';
+var rightAlert = ('That\'s right !');
+var wrongAlert = ('That\'s too bad, wrong. ');
+var randomNumhigh = 'That number is too high, try again.'
+var randomNumlow = 'That number is too low, try again'
+var trys = 1;
+var times = 0;
+var nextGuess;
+var userName;
+var questions = 7;
+//Array Section
+//answers array for the site visitors
+var userAnswersArray = []
+var questionsArray= [langQuestion,heightQuestion,loveCodingQuestion,locationQuestion,laptopQuestion];
+var correctAnswerArray = [langAnswer,heightAnswer,loveCodingAnswer,locationAnswer,laptopAnswer];
+var martialArtsAnswers = ['karate','jiu jitsu','taekwondo','military combatives'];
+//This is getting user's name and welcomes the user to the site
+alert('Hi, Welcome to paradise!!')
+userName = prompt('Just kidding, Enter your name.');
+
+alert('Welcome to my site lets have a short quiz followed by a guessing game.')
+for(var i = 0; i < questionsArray.length; i++){
+    userAnswersArray.push(prompt(questionsArray[i]).toLocaleLowerCase())
+    if(correctAnswerArray[i] === userAnswersArray[i]){
+      alert(rightAlert);
+      countRight++;
+    }
+    else if(correctAnswerArray[i] !== userAnswersArray[i].toLocaleLowerCase()){
+      alert(wrongAlert)
+      countWrong++;
+    }
+  }
+console.log(countRight +' Right');
+console.log(countWrong +' Wrong');
+
+
+// Guessing game starts!!
+alert('Now we will have a guessing game. You will be prompted to pick a number. You will be told if the number is too high or too low. You will get 4 chances to guess the correct answer.');
+//I've created a random number for the user to guess
+var randomNum = Math.floor(Math.random() *15);
+console.log('the number is ' + randomNum);
+var userNumberGuess = prompt('Guess a number from 1 - 15');
+parseInt(userNumberGuess, 10);
+
+while(times < 5){
+  
+  if(userNumberGuess < randomNum)
+  {
+    alert(randomNumlow);
+  }
+  else if(userNumberGuess > randomNum)
+  {
+    alert(randomNumhigh);
+  }
+  else{
+    alert('Awesome great guess');
+    countRight++;
+    break
+  }
+  userNumberGuess = prompt('Guess again!')
+  parseInt(userNumberGuess, 10);
+  console.log(userNumberGuess);
+  times++;
+}
+
+
+
+// This would be the final question that has multiple answers that could be guessed
+var martialArts = prompt('Can you guess one of the Martial Arts I\'ve learned, you only get 6 attempts.').toLowerCase()
+while(trys < 7)
+{
+  for(var j = 0; j < martialArtsAnswers.length; j++)
+  {
+    if(martialArtsAnswers[j] === martialArts)
+    {
+      alert(rightAlert);
+      j = martialArts.length;
+      flag = true;
+    }
+  }
+  if(flag === true)
+  {
+    countRight++;
+    break
+  }
+  martialArts = prompt('Guess again, attempt no. '+ trys).toLowerCase();
+  trys++
+}
+alert('All of the possible answers you could have said are '+martialArtsAnswers)
+
+
+var countRightPercent = Math.floor(10/ questions * countRight * 10);
+// countRightPercent = countRightPercent * 10;
+while(countRight !== 7)
+{
+  alert(userName +', You almost had it, You got ' +countRight+' out of 7 right that\'s '+ countRightPercent +'%')
+  break
+}
+if (countRight === 7){
+  alert(userName +', That\'s awesome you got the '+ countRight+' out of 7 right that\'s '+ countRightPercent +'%')
+}
+
